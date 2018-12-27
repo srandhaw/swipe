3ff177a54c730b79faabb02e9cd6c949b1c779d5
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-12-27 18:39:41 +0400

    pan responder and animation linked in deck

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,23 @@
 import React, {Component} from 'react'
-import {View, Animated} from 'react-native'
+import {View, Animated, PanResponder} from 'react-native'
 
 class Deck extends Component{
+
+    constructor(props){
+        super(props)
+
+        const position = new Animated.ValueXY()
+
+        const panResponder = PanResponder.create({
+         onStartShouldSetPanResponder: ()=>true,
+         onPanResponderMove: (event, gesture)=>{
+          position.setValue({x: gesture.dx,y: gesture.dy})
+         },
+         onPanResponderRelease: ()=>{},
+        })
+
+        this.state = {panResponder, position}
+    }
 
     renderCards(){
         return this.props.data.map((item)=>{
@@ -12,9 +28,12 @@ class Deck extends Component{
 
     render(){
         return(
-            <View>
+            <Animated.View 
+            style = {this.state.position.getLayout()}
+            {...this.state.panResponder.panHandlers}
+            >
             {this.renderCards()}
-            </View>
+            </Animated.View>
         )
     }
 }
