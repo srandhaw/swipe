2152809509048a4aea25556993687c85aa013204
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-12-30 17:30:09 +0400

    implemented animations as we go through stack

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {View, Animated, PanResponder, Dimensions} from 'react-native'
+import {View, Animated, PanResponder, Dimensions, LayoutAnimation, UIManager} from 'react-native'
 
 const SCREEN_WIDTH = Dimensions.get('window').width
 const SWIPE_THRESHOLD = 0.25*SCREEN_WIDTH
@@ -37,6 +37,17 @@ class Deck extends Component{
         })
 
         this.state = {panResponder, position, index: 0}
+    }
+
+    componentWillReceiveProps(nextProps){
+      if(nextProps.data !== this.props.data){
+          this.setState({index: 0})
+      }
+    }
+
+    componentWillUpdate(){
+      UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true)
+      LayoutAnimation.spring()
     }
 
     forceSwipe(direction){
@@ -102,7 +113,7 @@ class Deck extends Component{
                return (
                    <Animated.View 
                    key = {item.id} 
-                   style = {[styles.cardStyle,{top: 10*(i-this.state.index)}]}
+                   style = {[styles.cardStyle,{top: 10*(index-this.state.index)}]}
                    >
                    {this.props.renderCard(item)}
                    </Animated.View>
